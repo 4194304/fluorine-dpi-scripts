fdd5e0838c742c3eeb9814738c35e33003c469c3
4194304 <130800536+4194304@users.noreply.github.com>
2024-07-01 17:41:42 +0000

    setdpi
    the elephant in the room

---
+++ setdpi.sh
@@ -0,0 +1,19 @@
+if ! [[ "$1" =~ ^[0-9]+$ ]]
+    then
+        echo "Number is not a valid integer, try again."
+        exit
+fi
+if ! [[ "$2" =~ ^[0-9]+$ ]]
+    then
+        echo "Number is not a valid integer, try again."
+        exit
+fi
+
+mkdir ~/.fluorine
+rm -rf ~/.Xresources
+declare -i A=($1*96/100)
+echo Xft.dpi: $A >> ~/.Xresources
+rm -rf ~/.fluorine/dpi.sh
+declare -i B=($2*96/100)
+echo "echo Xft.dpi: $B | xrdb -override" >> ~/.fluorine/dpi.sh
+chmod +x ~/.fluorine/dpi.sh
